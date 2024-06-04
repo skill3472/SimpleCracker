b470fad168cdc85e87c009e4179c6cb66cd37c12
Maksymilian Tym <tymszczur@gmail.com>
2024-06-04 17:01:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+﻿## numcracker by skill347
+A simple Python script for cracking a range of numbers, from a hash pattern.
+
+---
+## Usage
+```
+usage: numcracker [-h] [--mode {sha512,sha256,sha1,md5}] [--out OUT] [--verbose] range expression
+
+Crack simple number hashes.
+
+positional arguments:
+  range                 Range of numbers to crack. Example: 100-250
+  expression            RegEx expression to match.
+
+options:
+  -h, --help            show this help message and exit
+  --mode {sha512,sha256,sha1,md5}, -m {sha512,sha256,sha1,md5}
+                        Select the hashing algorithm. Default is SHA512.
+  --out OUT, -o OUT     Output filename.
+  --verbose, -v         Verbose mode.
+```
+
---
+++ numcracker.py
@@ -0,0 +1,69 @@
+﻿import hashlib
+import argparse
+import re
+
+parser = argparse.ArgumentParser(
+    prog='numcracker',
+    description='Crack simple number hashes. Made by skill347'
+)
+
+parser.add_argument("range", type=str, help="Range of numbers to crack. Example: 100-250")
+parser.add_argument("expression", type=str, help="RegEx expression to match.")
+parser.add_argument("--mode", "-m", type=str, help="Select the hashing algorithm. Default is SHA512.",
+                    default="sha512", choices=["sha512", "sha256", "sha1", "md5"])
+parser.add_argument("--out", "-o", type=str, help="Output filename.")
+parser.add_argument("--verbose", "-v", action="store_true", help="Verbose mode.")
+
+args = parser.parse_args()
+startPos = int(args.range.split('-')[0])
+endPos = int(args.range.split('-')[1])
+exp = re.compile(args.expression)
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def HashNumber(number, mode="sha512") -> str:
+    mode = mode.lower()
+    if mode not in hashlib.algorithms_available:
+        raise ValueError("Hashing algorithm not supported on this version of the Python interpreter.")
+
+    match mode:
+        case "sha512":
+            return hashlib.sha512(str(number).encode()).hexdigest()
+        case "sha256":
+            return hashlib.sha256(str(number).encode()).hexdigest()
+        case "sha1":
+            return hashlib.sha1(str(number).encode()).hexdigest()
+        case "md5":
+            return hashlib.md5(str(number).encode()).hexdigest()
+        case _:
+            raise ValueError("Hashing mode does not exist, or is not supported.")
+
+
+found = []
+for i in range(startPos, endPos + 1):
+    if args.verbose:
+        print(f"Cracking number {i}/{endPos}...")
+    res = HashNumber(i, args.mode)
+    if re.match(exp, res):
+        if args.out:
+            with open(args.out, 'w') as f:
+                f.write(str(i))
+                f.write("\n")
+                f.close()
+        found.append(str(i))
+
+if found:
+    print(f"{bcolors.OKGREEN}Numbers that match the provided expression: {found}{bcolors.ENDC}")
+else:
+    print(f"{bcolors.FAIL}No numbers that match the provided expression have been found.{bcolors.ENDC}")
